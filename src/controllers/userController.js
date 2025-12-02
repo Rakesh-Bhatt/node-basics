5b3cb4fe2d578bc1a6aefa6485295b83b1beca01
Rakesh Bhatt <rakeshbhatt@bajratechnologies.com>
2025-12-02 23:11:51 +0545

    feat: add new controller action to get the user by id from parameter

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,3 +8,20 @@ exports.getUsers = (req, res, next) => {
     next(err);
   }
 };
+
+exports.getUserByID = (req, res, next) => {
+  try{
+    const user = userService.getUserByID(Number(req.params.id));
+
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404)
+      res.json({
+        message: "User not found by given Id"
+      });
+    }
+  } catch(err) {
+    next(err);
+  }
+}
