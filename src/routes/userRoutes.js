62ae27ca60713365962d380417950c429b72406a
Rakesh Bhatt <rakeshbhatt@bajratechnologies.com>
2025-12-09 22:40:16 +0545

    feat: add joi for validating the user input

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,10 +8,12 @@ const {
   deleteUser
  } = require('../controllers/userController');
 
+const { validateUser } = require('../middleware/validateUser');
+
 router.get("/users", getUsers);
 router.get("/users/:id", getUserByID);
-router.post("/users", addNewUser);
-router.put("/users/:id", updateUser)
+router.post("/users",validateUser, addNewUser);
+router.put("/users/:id", validateUser, updateUser)
 router.delete("/users/:id", deleteUser)
 
 module.exports = router;
